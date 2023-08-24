7ad4d63c853d0ec4d0fea6cfd6604d059d81fa31
agrova <kseniya.agrova@gmail.com>
2023-08-24 14:45:01 +0400

    Сделана перестановка блоков по логике

--- utils.py
+++ utils.py
@@ -1,16 +1,69 @@
 from urllib.parse import urlencode
 import pandas as pd
+import numpy as np
 
 import requests
 
+BASE_URL = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?' 
 
-def download_data(base_url, public_key, separ):
+
+def download_data(public_key, local_path = None, separ = ';'):
     # Получаем доп.таблицу с группами
- 
     # получаем url
-    final_url = base_url + urlencode(dict(public_key=public_key))
+    final_url = BASE_URL + urlencode(dict(public_key=public_key))
     response = requests.get(final_url)
-    download_url_orders = response.json()['href']
+    if response.ok:
+        download_url_orders = response.json()['href']
+        return pd.read_csv(download_url_orders, delimiter=separ)
+
+    return pd.read_csv(local_path, delimiter=separ)
+
+
+def collect_summary_df(groups_df, groups_add_df, active_studs_df, checks_df):
     
-    # загружаем файл в df
-    return pd.read_csv(download_url_orders, delimiter=separ)
+    groups_full_df = pd.DataFrame(np.concatenate((groups_df.values, groups_add_df.values), axis=0))
+    groups_full_df.columns = [ 'id', 'grp']
+    groups_full_df.drop_duplicates()
+    # groups_full_df = pd.concat([groups_df, groups_add_df])
+
+    # Получаем таблицу с юзерами, которые были на платформе в день эксперимента
+    experiment_users = pd.merge(groups_full_df, active_studs_df, how='right', left_on='id', right_on='student_id').drop(
+    'student_id', axis=1)
+    # Соединяем клиентов из эксперимента с df чеками
+    summary_df = pd.merge(checks_df, experiment_users, how='right', left_on='student_id', right_on='id').drop('student_id', axis=1)
+    summary_df['rev'] = summary_df['rev'].fillna(0)
+    # Создаем столбец лейб - конверсии
+    summary_df['converted'] = np.where(summary_df.rev > 0, 1, 0)
+    
+    return summary_df
+
+def consolidation_df(experiment_df):
+
+    # Считаем конвесии на каждого пользователя (на случай если есть повторные покупки)
+    experiment_df_cr = (
+    experiment_df.groupby(['grp', 'id'])
+    .agg({'converted': 'sum'})
+    .reset_index()
+    .sort_values('converted', ascending=False)
+)
+    # Считаем отдельно конверсии = 0 и 1
+    cr_df = experiment_df_cr.pivot_table(index='grp', columns='converted', aggfunc='size', fill_value=0)
+    cr_df.columns = ['converted_0', 'converted_1']
+    cr_df = cr_df.reset_index()
+    cr_df['total_conversions'] = cr_df['converted_0'] + cr_df['converted_1']
+    # Получаем CR
+    cr_df['conversion_rate'] = cr_df['converted_1'] / cr_df['total_conversions']
+    # считаем доход по группам
+    rev_4_grp_df = experiment_df.groupby('grp').agg(rev=('rev', 'sum'), users=('id', 'nunique')).reset_index()
+    # считаем количество платящих пользователей
+    paying_users_df = (
+    experiment_df_cr.query('converted==1').reset_index().groupby('grp').agg(paying_users=('id', 'nunique')).reset_index()
+)
+    # Добавляем к df столбцы с доходо и количеством пользователей
+    consolidated_df = (cr_df.merge(rev_4_grp_df, on='grp')).merge(paying_users_df, on='grp')
+    # # Считаем ARPAU
+    consolidated_df['arpau'] = consolidated_df['rev'] / consolidated_df['users']
+    # Считаем ARPPU
+    consolidated_df['arppu'] = consolidated_df['rev'] / consolidated_df['paying_users']
+
+    return consolidated_df
